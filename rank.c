e80956b970f994ce30ee024e85ab8fcba150eb5d
Yirna <62490216+Yirna@users.noreply.github.com>
2022-06-01 11:31:51 +0900

    Add files via upload

---
+++ rank.c
@@ -0,0 +1,28 @@
+#include "header.h"
+
+
+
+void rank(int rank[], char player_name[]) {
+	text_align_center(HOR, "졸업기록 열람\n\n\n");
+	if (rank[0] == 0) {
+		printf("학사 취득 기록이 존재하지 않습니다");
+	}
+	else {
+		text_align_center(HOR, "%s님의 학사 취득 기록 : %d 학기\n", player_name, rank[0]);
+	}
+	if (rank[1] == 0) {
+		printf("석사 취득 기록이 존재하지 않습니다");
+	}
+	else {
+		text_align_center(HOR, "%s님의 석사 취득 기록 : %d 학기\n", player_name, rank[1]);
+	}
+	if (rank[2] == 0) {
+		printf("박사 취득 기록이 존재하지 않습니다");
+	}
+	else {
+		text_align_center(HOR, "%s님의 박사 취득 기록 : %d 학기\n", player_name, rank[2]);
+	}
+
+
+
+}

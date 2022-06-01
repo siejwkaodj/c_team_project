18ec9c6b378ff2ad44e5e931123ac49895b4bd50
siejwkaodj <54460736+siejwkaodj@users.noreply.github.com>
2022-06-01 11:30:31 +0900

    0601 / hsj / detection수정, main에 game_start변수 추가, TODO - 점수구현, 교수/패널티시 동작 구현, 게임 종료시 점수계산하는로직

--- detection.c
+++ detection.c
@@ -1,6 +1,5 @@
 #include "header.h"
 extern map[50][50];
-<<<<<<< HEAD
 extern min_x, min_y, max_x, max_y;
 // 빈 칸 = 0 / 장애물 = 2 / 교수 = 3 / 깃발 - 4 / 보물 - 5
 // 움직일 다음 칸에 뭐가 있는지 반환해줌.
@@ -25,49 +24,3 @@ int detection(int x, int y, int ch) {
 		return 2;
 	return map[y][x];
 }
-=======
-
-int detect(int x, int y) {
-	if (map[x][y] == "▩")
-		return 0;
-	else if (map[x][y] == "◎")
-		return 1;
-	else if (map[x][y] == "▶")
-		return 2;
-	else
-		return 3;
-}
-// 장애물 = 0 / 교수 = 1 / 깃발 및 보물 = 2 / 아무것도 없는칸 = 3
-void detection(int x,int y, int ch) {
-	int detectional;
-	switch (ch)
-	{
-	case 72:
-		
-			y--;
-			detectional = detect(x, y);
-			y++;
-		break;
-	case 80:
-			
-			y++;
-			detectional = detect(x, y);
-			y--;
-		break;
-	case 75:
-	
-			x -= 2;
-			detectional = detect(x, y);
-			x += 2;
-		break;
-	case 77:
-	
-			x += 2;
-			detectional = detect(x, y);
-			x += 2;
-		break;
-	}
-	return detectional;
-}
-	// 이동할 칸에 무엇이 있는지 판정후 좌표를 다시 제자리로 이동함.
->>>>>>> 4146fafbd2405d9d33cc4af72bfd43ac7f03a668
